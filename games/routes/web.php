0e11d92288af5af283f9c4a7c15f9a7fd9466b8d
Chloe <2226477@talnet.nl>
2026-09-04 12:01:24 +0200

    Read: controller, view en route

--- games/routes/web.php
+++ games/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });

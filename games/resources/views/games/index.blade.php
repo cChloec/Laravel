c795c9eaf6786ac7fe750c7da643fd7ea5185f1f
Chloe <2226477@talnet.nl>
2026-09-04 12:03:51 +0200

    Add genre to games view

--- games/resources/views/games/index.blade.php
+++ games/resources/views/games/index.blade.php
@@ -9,22 +9,26 @@
 <body>
     <div class="container" style="margin:40px;">
         <h1 class="display-4">🎮 Game Collection</h1>
+
         <table class="table">
             <thead class="thead-dark">
                 <tr>
                     <th>ID</th>
                     <th>Game</th>
                     <th>Platform</th>
+                    <th>Genre</th>
                     <th>Rating</th>
                 </tr>
             </thead>
+
             <tbody>
-                @foreach($games as $game) {{-- in deze loop worden alle rijen (records) gemaakt die in de database zijn gevonden. --}}
+                @foreach($games as $game)
                     <tr>
                         <td>{{ $game->id }}</td>
                         <td>{{ $game->game_name }}</td>
                         <td>{{ $game->platform }}</td>
-                        <td>{{ $game->rating }}/10</td> 
+                        <td>{{ $game->genre }}</td>
+                        <td>{{ $game->rating }}/10</td>
                     </tr>
                 @endforeach
             </tbody>

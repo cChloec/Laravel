97d6f06d557b5375edb7ae56918e1d2c8ac02d9c
Chloe <2226477@talnet.nl>
2026-09-11 11:42:17 +0200

    Create: formulier en store

--- games/app/Http/Controllers/GameController.php
+++ games/app/Http/Controllers/GameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Game;
 use Illuminate\Http\Request;
 
+
 class GameController extends Controller
 {
     /**
@@ -18,18 +19,34 @@ public function index()
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+public function create()
+{
+    return view('games.create');
+}
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+public function store(Request $request)
+{
+    $request->validate([
+        'game_name' => 'required',
+        'platform' => 'required',
+        'genre' => 'required',
+        'rating' => 'required|numeric|min:0|max:10'
+    ]);
+
+    $game = new Game([
+        'game_name' => $request->get('game_name'),
+        'platform' => $request->get('platform'),
+        'genre' => $request->get('genre'),
+        'rating' => $request->get('rating')
+    ]);
+
+    $game->save();
+
+    return redirect('/games')->with('success', 'Game added!');
+}
 
     /**
      * Display the specified resource.
--- games/routes/web.php
+++ games/routes/web.php
@@ -7,3 +7,5 @@ Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
+Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
